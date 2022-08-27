c10093e2bafc29abe85b931174f9b3e5e178d05a
Adi <adib09132005@gmail.com>
2022-08-26 21:54:39 -0500

    Test shiftByN method

--- src/Runner.java
+++ src/Runner.java
@@ -6,6 +6,8 @@ public class Runner {
         System.out.println(methods.doubleUp(makeStack(new int[] { 1, 3, 5, 0, -1 })));
 
         System.out.println(methods.posAndNeg(makeStack(new int[] { 2, 9, -4, 3, -1, 0, -6 })));
+    
+        System.out.println(methods.shiftByN(makeStack(new int[] {7, 23, -7, 0, 22, -8, 4, 5}), 3));
     }
 
     public static Stack<Integer> makeStack(int[] arr) {
